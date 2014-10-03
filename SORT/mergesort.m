4fe065b69ac2931aca4d24a1cacf34f10d355e61
Aurametrix <irene@aurametrix.com>
2014-10-02 20:04:32 -0700

    Merge Sort in matlab

---
+++ SORT/mergesort.m
@@ -0,0 +1,50 @@
+function s = mysort(a)
+
+
+% sort vector a into nodecreasing order.
+
+n = length(a);
+
+if n <= 1
+
+s = a;
+
+else
+
+s = merge( mysort( a(1 : n/2) ), ...
+
+mysort( a(n/2+1 : n) ) );
+
+end
+
+function m = merge(a,b)
+
+% Given sorted vectors a and b,
+
+% return sort([a,b]).
+
+n = length(a) + length(b);
+
+m = zeros(1,n);
+
+a = [a, inf]; b = [b, inf];
+
+ia = 1; ib = 1;
+
+for i = 1:n
+
+if a(ia) < b(ib)
+
+m(i) = a(ia);
+
+ia = ia + 1;
+
+else
+
+m(i) = b(ib);
+
+ib = ib + 1;
+
+end
+
+end
