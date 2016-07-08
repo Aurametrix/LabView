73f20ddebcf89ab375bccee5bd4cf5bafab65a52
Aurametrix <irene@aurametrix.com>
2016-07-07 20:45:28 -0700

    Use Bagged Decision Trees to Classify the Ionosphere Data

--- ML/clssf.m
+++ ML/clssf.m
@@ -7,3 +7,11 @@ Xtrain = X(training(cv),:);
 Ytrain = Y(training(cv));
 Xtest = X(test(cv),:);
 Ytest = Y(test(cv));
+
+% Classification Tree is chosen as the learner
+mdl1 = ClassificationTree.template('NVarToSample','all');
+RF1 = fitensemble(Xtrain,Ytrain,'Bag',150,mdl1,'type','classification');
+
+% Classification Tree with surrogate splits is chosen as the learner
+mdl2 = ClassificationTree.template('NVarToSample','all','surrogate','on');
+RF2 = fitensemble(Xtrain,Ytrain,'Bag',150,mdl2,'type','classification');
